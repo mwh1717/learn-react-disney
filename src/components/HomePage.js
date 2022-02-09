240d2750c0854d3bb4fe90a7804d847b83cc89a2
mwh1717 <michael.wells.howell@gmail.com>
2022-02-08 19:19:39 -0500

    CHECKPOINT: homepage background complete

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function HomePage() {
 	return (
-		<div>
+		<HomeContainer>
 			HOME PAGE
-		</div>
+		</HomeContainer>
 	)
 }
 
 export default HomePage
+
+const HomeContainer = styled.main`
+	min-height: calc(100vh - 70px);
+	position: relative;
+	
+	&:before {
+		background: url("/images/home-background.png") center center / cover no-repeat fixed;
+		content: "";
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		z-index: -10;
+	}
+`
